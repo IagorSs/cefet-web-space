15ae6527afe1d80ea8308f6cce6673473e918853
IagorSs <iagor.penaranda@uol.com.br>
2021-11-23 13:34:27 -0300

    Exercice 1 done

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,14 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const { value: constant } = document.getElementById('constante');
+
+const calcButton = document.getElementById('calcular');
+calcButton.onclick = () => {
+  const { value: mass1 } = document.getElementById('massa1');
+  const { value: mass2 } = document.getElementById('massa2');
+  const { value: dist } = document.getElementById('distancia');
+  
+  const resultElement = document.getElementById('resultado');
+  resultElement.value = (constant * mass1 * mass2) / (dist**2);
+}
