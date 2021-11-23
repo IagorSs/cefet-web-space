45760a19d0bd4c13edda9f9c1c8f388af824aa34
IagorSs <iagor.penaranda@uol.com.br>
2021-11-23 14:09:50 -0300

    Exercice 3 done

--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,16 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+const expandClass = 'expandido'
+const buttonsExpand = document.querySelectorAll('.botao-expandir-retrair');
+
+buttonsExpand.forEach(button => {
+  const { parentNode: paragraph } = button;
+
+  button.onclick = () => {
+    const willBeExpanded = !paragraph.classList.contains(expandClass);
+    
+    button.textContent = willBeExpanded ? '-':'+';
+    paragraph.classList.toggle(expandClass);
+  }
+});
